3b570c04b2e3b55b46455b244fd450400b3e28d5
JRaspat <73627514+JRaspat-BlackNode@users.noreply.github.com>
2023-12-30 14:55:08 -0500

    Update jito.ts

--- src/clients/jito.ts
+++ src/clients/jito.ts
@@ -1,39 +1,47 @@
 import { Keypair } from '@solana/web3.js';
 import { config } from '../config.js';
 import { geyserClient as jitoGeyserClient } from 'jito-ts';
-
-import {
-  SearcherClient,
-  searcherClient as jitoSearcherClient,
-} from 'jito-ts/dist/sdk/block-engine/searcher.js';
+import { SearcherClient, searcherClient as jitoSearcherClient } from 'jito-ts/dist/sdk/block-engine/searcher.js';
 import * as fs from 'fs';
 
+// Constants and configurations
 const BLOCK_ENGINE_URLS = config.get('block_engine_urls');
 const AUTH_KEYPAIR_PATH = config.get('auth_keypair_path');
-
 const GEYSER_URL = config.get('geyser_url');
 const GEYSER_ACCESS_TOKEN = config.get('geyser_access_token');
 
-const decodedKey = new Uint8Array(
-  JSON.parse(fs.readFileSync(AUTH_KEYPAIR_PATH).toString()) as number[],
-);
-const keypair = Keypair.fromSecretKey(decodedKey);
-
-const searcherClients: SearcherClient[] = [];
-
-for (const url of BLOCK_ENGINE_URLS) {
-  const client = jitoSearcherClient(url, keypair, {
-    'grpc.keepalive_timeout_ms': 4000,
-  });
-  searcherClients.push(client);
+// Asynchronous function to read and parse the authentication keypair
+async function readAuthKeypair(filePath: string): Promise<Keypair> {
+  try {
+    const fileContent = await fs.promises.readFile(filePath, 'utf-8');
+    const decodedKey = new Uint8Array(JSON.parse(fileContent) as number[]);
+    return Keypair.fromSecretKey(decodedKey);
+  } catch (error) {
+    console.error('Error reading authentication keypair:', error);
+    throw error;
+  }
 }
 
-const geyserClient = jitoGeyserClient(GEYSER_URL, GEYSER_ACCESS_TOKEN, {
-  'grpc.keepalive_timeout_ms': 4000,
-});
+// Asynchronously initialize all searcher clients
+async function initializeSearcherClients(urls: string[], keypair: Keypair): Promise<SearcherClient[]> {
+  const clientOptions = { 'grpc.keepalive_timeout_ms': 4000 };
+  const clients = urls.map(url => jitoSearcherClient(url, keypair, clientOptions));
+  return Promise.all(clients);
+}
 
-// all bundles sent get automatically forwarded to the other regions.
-// assuming the first block engine in the array is the closest one
-const searcherClient = searcherClients[0];
+async function initializeClients() {
+  try {
+    const keypair = await readAuthKeypair(AUTH_KEYPAIR_PATH);
+    const searcherClients = await initializeSearcherClients(BLOCK_ENGINE_URLS, keypair);
+    const geyserClient = jitoGeyserClient(GEYSER_URL, GEYSER_ACCESS_TOKEN, { 'grpc.keepalive_timeout_ms': 4000 });
 
-export { searcherClient, searcherClients, geyserClient };
+    // Assuming the first block engine is the closest one
+    const primarySearcherClient = searcherClients[0];
+    return { primarySearcherClient, searcherClients, geyserClient };
+  } catch (error) {
+    console.error('Error initializing clients:', error);
+    throw error;
+  }
+}
+
+export { initializeClients };
